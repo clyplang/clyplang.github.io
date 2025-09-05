4046885316925594aa67136b15488104bbfcc242
CodeSoftGit <137560679+CodeSoftGit@users.noreply.github.com>
2025-09-05 16:46:50 -0400

    Optimization

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,12 @@
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
+import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
-  integrations: [react()],
+  integrations: [react(), sitemap()],
   output: 'static',
   build: {
     assets: 'assets'
   },
-  site: "https://clyplang.github.com"
+  site: "https://clyp.dev"
 });
